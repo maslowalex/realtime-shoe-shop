5518e750908c5f22b014236acc9f0978f637f165
maslowalex <maslo.rails@gmail.com>
2020-08-29 17:09:24 +0300

    Fix sell item

--- lib/sneakers_23/checkout/single_item.ex
+++ lib/sneakers_23/checkout/single_item.ex
@@ -1,4 +1,3 @@
-
 defmodule Sneakers23.Checkout.SingleItem do
   alias Sneakers23.Inventory
   alias Inventory.Store
@@ -7,7 +6,10 @@ defmodule Sneakers23.Checkout.SingleItem do
     inventory_opts = Keyword.get(opts, :inventory_opts, [])
 
     with {:db, :ok} <- {:db, Store.reduce_availability(item_id)} do
-      Task.start_link(fn -> Inventory.item_sold!(item_id, inventory_opts) end)
+      unless Keyword.get(opts, :skip_replication, false) do
+        Task.start_link(fn -> Inventory.item_sold!(item_id, inventory_opts) end)
+      end
+
       :ok
     else
       {:db, e = {:error, _}} ->
